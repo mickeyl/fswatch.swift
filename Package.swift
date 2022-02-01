b79e5e23046e77aad550fce367aabf2289fe82d4
Dr. Michael Lauer <mickey@vanille-media.de>
2022-02-01 16:33:47 +0100

    Package: add brew and apt providers

--- Package.swift
+++ Package.swift
@@ -27,7 +27,14 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .systemLibrary(name: "libfswatch", pkgConfig: "libfswatch"),
+        .systemLibrary(
+            name: "libfswatch",
+            pkgConfig: "libfswatch",
+            providers: [
+                .brew(["fswatch"]),
+                .apt(["fswatch"])
+            ]            
+        ),
         .target(
             name: "fswatch",
             dependencies: ["libfswatch"]),
